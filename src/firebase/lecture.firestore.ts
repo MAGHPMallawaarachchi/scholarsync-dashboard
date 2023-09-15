6ed5337d70605d692213899d6e08c6cd137a3527
Hasini Punsala <hasinipunsala@gmail.com>
2023-09-15 13:09:50 +0530

    chore: create CRUD for models

---
+++ src/firebase/lecture.firestore.ts
@@ -0,0 +1,86 @@
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    where,
+    getDocs,
+    doc,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+  } from 'firebase/firestore';
+import Lecture from '@/models/Lecture';
+import { firestore } from './config';
+  
+  // Create a new lecture
+  export async function createLecture(lecture: Lecture): Promise<void> {
+    try {
+      // Add a new document to the "lectures" collection
+      await addDoc(collection(firestore, 'lectures'), lecture);
+    } catch (error) {
+      console.error('Error creating lecture:', error);
+      throw error;
+    }
+  }
+  
+  // Read all lectures
+  export async function getAllLectures(): Promise<Lecture[]> {
+    try {
+      const lectures: Lecture[] = [];
+      const querySnapshot = await getDocs(collection(firestore, 'lectures'));
+  
+      querySnapshot.forEach((doc) => {
+        lectures.push({ id: doc.id, ...doc.data() } as Lecture);
+      });
+  
+      return lectures;
+    } catch (error) {
+      console.error('Error getting lectures:', error);
+      throw error;
+    }
+  }
+  
+  // Read a single lecture by ID
+  export async function getLectureById(id: string): Promise<Lecture | null> {
+    try {
+      const docSnapshot = await getDoc(doc(firestore, 'lectures', id));
+  
+      if (docSnapshot.exists()) {
+        return { id: docSnapshot.id, ...docSnapshot.data() } as Lecture;
+      }
+  
+      return null;
+    } catch (error) {
+      console.error('Error getting lecture by ID:', error);
+      throw error;
+    }
+  }
+  
+  // Update an existing lecture by ID
+  export async function updateLectureById(
+    id: string,
+    updatedLecture: Partial<Lecture>
+  ): Promise<void> {
+    try {
+      const lectureRef = doc(firestore, 'lectures', id);
+  
+      await updateDoc(lectureRef, updatedLecture);
+    } catch (error) {
+      console.error('Error updating lecture by ID:', error);
+      throw error;
+    }
+  }
+  
+  // Delete a lecture by ID
+  export async function deleteLectureById(id: string): Promise<void> {
+    try {
+      const lectureRef = doc(firestore, 'lectures', id);
+  
+      await deleteDoc(lectureRef);
+    } catch (error) {
+      console.error('Error deleting lecture by ID:', error);
+      throw error;
+    }
+  }
+  
---
+++ src/firebase/lecturer.firestore.ts
@@ -0,0 +1,88 @@
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    where,
+    getDocs,
+    doc,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+  } from 'firebase/firestore';
+
+import Lecturer from '@/models/Lecturer';
+import { firestore } from './config';
+  
+  
+  // Create a new lecturer
+  export async function createLecturer(lecturer: Lecturer): Promise<void> {
+    try {
+      // Add a new document to the "lecturers" collection
+      await addDoc(collection(firestore, 'lecturers'), lecturer);
+    } catch (error) {
+      console.error('Error creating lecturer:', error);
+      throw error;
+    }
+  }
+  
+  // Read all lecturers
+  export async function getAllLecturers(): Promise<Lecturer[]> {
+    try {
+      const lecturers: Lecturer[] = [];
+      const querySnapshot = await getDocs(collection(firestore, 'lecturers'));
+  
+      querySnapshot.forEach((doc) => {
+        lecturers.push({ id: doc.id, ...doc.data() } as Lecturer);
+      });
+  
+      return lecturers;
+    } catch (error) {
+      console.error('Error getting lecturers:', error);
+      throw error;
+    }
+  }
+  
+  // Read a single lecturer by ID
+  export async function getLecturerById(id: string): Promise<Lecturer | null> {
+    try {
+      const docSnapshot = await getDoc(doc(firestore, 'lecturers', id));
+  
+      if (docSnapshot.exists()) {
+        return { id: docSnapshot.id, ...docSnapshot.data() } as Lecturer;
+      }
+  
+      return null;
+    } catch (error) {
+      console.error('Error getting lecturer by ID:', error);
+      throw error;
+    }
+  }
+  
+  // Update an existing lecturer by ID
+  export async function updateLecturerById(
+    id: string,
+    updatedLecturer: Partial<Lecturer>
+  ): Promise<void> {
+    try {
+      const lecturerRef = doc(firestore, 'lecturers', id);
+  
+      await updateDoc(lecturerRef, updatedLecturer);
+    } catch (error) {
+      console.error('Error updating lecturer by ID:', error);
+      throw error;
+    }
+  }
+  
+  // Delete a lecturer by ID
+  export async function deleteLecturerById(id: string): Promise<void> {
+    try {
+      const lecturerRef = doc(firestore, 'lecturers', id);
+  
+      await deleteDoc(lecturerRef);
+    } catch (error) {
+      console.error('Error deleting lecturer by ID:', error);
+      throw error;
+    }
+  }
+  
